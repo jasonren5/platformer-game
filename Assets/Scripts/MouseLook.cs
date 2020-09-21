a5c6e9465a0fd7f8a705df31815e3c76119cbdbe
jasonren5 <jasonren72@gmail.com>
2020-09-21 17:13:58 -0500

    added basic mouselook and player movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,16 +8,34 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerTransform;
 
+    float rotationX = 0f;
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //prevent mouse from moving while looking around
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        handleMouseMovement();
+    }
+
+    //mouse look camera movement 
+    void handleMouseMovement()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //camera x rotation is up and down; clamp it so your camera cannot do flips
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.right);
+
+        //rotate playerTransform to look left and right
+        playerTransform.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float playerAccel = 12f;
+
+    CharacterController controller;
+    //Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleMovement();
     }
+
+    void handleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = x * transform.right * playerAccel + z * transform.forward * playerAccel;
+        //velocity = velocity + new Vector3(x * playerAccel, 0, z * playerAccel) * Time.deltaTime;
+        controller.Move(movement * Time.deltaTime);
+    }
+
+    
 }
